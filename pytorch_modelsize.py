e229a9e5534108b95bb88bec44a9789a119167df
Ирина Абдуллаева <a.irene.a@mail.ru>
2022-01-13 22:40:43 +0300

    model size on cuda estimation

--- pytorch_modelsize.py
+++ pytorch_modelsize.py
@@ -72,6 +72,25 @@ class SizeEstimator(object):
             self._model.cuda(device=self._model_init_device_id)
             logging.info(f"Model transferred to CUDA id #{self._model_init_device_id}.")
 
+    @staticmethod
+    def __check_device_available(device_id: int = 0):
+        """
+        Returns a bool indicating if CUDA with provided id is currently available.
+        """
+        if torch.cuda.is_available():
+            logging.info(f"CUDA device is available.")
+            if device_id in list(range(torch.cuda.device_count())):
+                try:
+                    torch.cuda.set_device(device_id)
+                except RuntimeError as e:
+                    logging.error(f"CUDA error: cannot set device with provided id.")
+                    return False
+                return True
+            else:
+                logging.error(f"CUDA error: invalid device id.")
+        logging.info(f"CUDA device #{device_id} is not available. Check configuration and try again.")
+        return False
+
     def _get_parameter_sizes(self) -> List[Parameter]:
         """
         Get sizes of all parameters in `model`.
@@ -179,6 +198,73 @@ class SizeEstimator(object):
         Estimate ONLY model weights size in memory in megabytes and bits.
         """
         total = self._input_weight
+        total_bytes = (total / 8)
+        total_megabytes = total_bytes / (1024 ** 2)
+        logging.info(f"Model size is: {total} bits, {total_bytes} bytes, {total_megabytes} Mb.")
+        return total_megabytes
+
+    def estimate_cuda_memory(self, device_id: int, optimizer_type: torch.optim.Optimizer,
+                             mixed_precision: bool = False) -> float:
+        """
+        Predict the maximum memory usage of the model,
+        which is placed on CUDA device.
+        GPU usage consist of three main components:
+            * Model parameters: the actual weights in network;
+            * Input: the main parameter of it is a batch size;
+            * Intermediate variables:
+                * for forward pass: activations of each weight;
+                * for backward pass: gradients for each weight;
+            * Optimizer: estimations of the first/second moments
+                        of the gradient, for each model weight.
+        Args:
+            optimizer_type (Type): the class name of the optimizer to instantiate;
+            mixed_precision (bool): whether to estimate based on using mixed precision;
+            device_id (int): the device to use.
+        """
+        # Reset model and optimizer
+        self.__restore_device()
+        available_flg = SizeEstimator.__check_device_available(device_id)
+        if not available_flg:
+            logging.error(f"Cannot estimate memory usage of the model on CUDA, because it is not available.")
+            return 0.0
+        device = torch.cuda.device(f'cuda:{device_id}')
+        # Releases all unoccupied cached memory currently held by the caching allocator
+        torch.cuda.empty_cache()
+        optimizer = optimizer_type(self._model.parameters(), lr=.001)
+
+        # 1. Model weights size
+        mem_0 = torch.cuda.memory_allocated(device_id)
+        self._model.to(f'cuda:{device_id}')
+        mem_1 = torch.cuda.memory_allocated(device)
+        model_memory = mem_1 - mem_0
+
+        # 2. Forward pass
+        model_input = torch.FloatTensor(*self._input_size)
+        output = self._model(model_input.to(f'cuda:{device_id}'))
+        mem_2 = torch.cuda.memory_allocated(device)
+
+        # 3. Backward pass
+        if mixed_precision:
+            mem_multiplier = .5
+        else:
+            mem_multiplier = 1
+        forward_pass_memory = (mem_2 - mem_1) * mem_multiplier
+        gradient_memory = model_memory
+
+        # 4. Optimizer memory usage
+        if isinstance(optimizer, torch.optim.Adam):
+            o = 2
+        elif isinstance(optimizer, torch.optim.RMSprop):
+            o = 1
+        elif isinstance(optimizer, torch.optim.SGD):
+            o = 0
+        else:
+            raise ValueError(f"""Unsupported optimizer type. Look up how many moments are 
+                             stored by your optimizer and add a case to the optimizer checker.""")
+        gradient_moment_memory = o * gradient_memory
+
+        # Total
+        total = model_memory + forward_pass_memory + gradient_memory + gradient_moment_memory
         total_bytes = (total / 8)
         total_megabytes = total_bytes / (1024 ** 2)
         logging.info(f"Model size is: {total} bits, {total_bytes} bytes, {total_megabytes} Mb.")
